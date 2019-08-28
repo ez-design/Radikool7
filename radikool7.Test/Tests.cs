c2bfe8aa7a2cceb66cff2058dc99d49fc4f6b9c7
ez-design <info@ez-deign.net>
2019-08-28 19:11:05 +0900

    ファイル追加

---
+++ radikool7.Test/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace radikool7.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
